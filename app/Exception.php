f3f4bad1cff74889d316d50cd32764b002e30873
JulyXing <julyxing@163.com>
2018-11-04 23:08:49 +0800

    初始化项目结构。

---
+++ app/Exception.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * 定义基础抽象异常类。
+ * 
+ * @author JulyXing <julyxing@163.com>
+ * @license GPL-3.0 +
+ * @copyright JulyXing
+ */
+
+namespace app;
+
+use Exception as PHPException;
+
+/**
+ * 抽象异常基类。
+ */
+abstract class Exception extends PHPException
+{
+
+}
